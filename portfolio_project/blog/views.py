e640f10f454a18bbb656b5cecf52fe62235e2448
LAPTOP-CEKV0UP6\edwin <edwinwen75@gmail.com>
2021-03-20 19:34:20 -0400

    django updated web

--- portfolio_project/blog/views.py
+++ portfolio_project/blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def blog(request):
-    info = 'high welcome to the blog'
+    info = 'hi welcome to the blog'
     return render(request, 'blogs/blog.html', {'info':info})
